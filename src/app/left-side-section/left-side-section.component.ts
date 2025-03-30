35eee99bb7b17f6735b367fd05391b555160e349
user <user@DESKTOP-1U118DG>
2025-03-30 15:49:08 +0300

    *ngIf + *ngFor

--- src/app/left-side-section/left-side-section.component.ts
+++ src/app/left-side-section/left-side-section.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {NgFor, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-left-side-section',
   // imports: [],
+  imports: [
+    NgFor,
+    NgIf],
   templateUrl: './left-side-section.component.html',
   standalone: true,
   styleUrl: './left-side-section.component.scss'
 })
-export class LeftSideSectionComponent {}
+export class LeftSideSectionComponent {
+  //директиви *ngIf
+  showContact = true;
+
+  toggleContact() {
+    this.showContact = !this.showContact;
+  }
+//директиви *ngFor
+  education = [
+    { year: '2005-2007', degree: 'Degree / Major Name', institution: 'Your College name here' },
+    { year: '2008-2010', degree: 'Degree / Major Name', institution: 'Another College' }
+  ];
+}
+
