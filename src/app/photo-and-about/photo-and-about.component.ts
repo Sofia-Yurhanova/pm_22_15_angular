03953b6144c3c18b60dd60743ef2b2bfd6593395
user <user@DESKTOP-1U118DG>
2025-04-06 20:15:53 +0300

    api not finished

--- src/app/photo-and-about/photo-and-about.component.ts
+++ src/app/photo-and-about/photo-and-about.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ResumeService } from '../resume.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-photo-and-about',
   templateUrl: './photo-and-about.component.html',
   standalone: true,
-  styleUrl: './photo-and-about.component.scss'
+  styleUrls: ['./photo-and-about.component.scss'],
+  imports: [FormsModule]
 })
-export class PhotoAndAboutComponent {}
+export class PhotoAndAboutComponent implements OnInit{
+  aboutText: string = '';
+  newAbout: string = '';
+
+  constructor(private resumeService: ResumeService) {}
+  ngOnInit() {
+    this.resumeService.getAbout().subscribe({
+      next: (data:any) => this.aboutText = data.body,
+      error: (err:any) => console.error(err)
+    });
+  }
+
+  sendNewText() {
+    if (!this.newAbout.trim()) return;
+    this.resumeService.sendAbout(this.newAbout).subscribe({
+      next: () => {
+        this.aboutText = this.newAbout;
+        this.newAbout = '';
+      }
+    });
+  }
+
+
+  // ngOnInit() {
+  //   this.ResumeService.getAboutText().subscribe({
+  //     next: (text:any) => this.aboutText = text,
+  //     error: (err:any) => {
+  //       console.error(err);
+  //       this.aboutText = 'Помилка завантаження тексту';
+  //     }
+  //   });
+  // }
+}
+
---
+++ src/app/resume.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResumeService {
+  private getUrl = 'http://localhost:3000/about'; // GET
+  private postUrl = 'http://localhost:3000/about'; // POST
+
+  constructor(private http: HttpClient) {}
+
+  getAbout(): Observable<{ body: string }> {
+    return this.http.get<{ body: string }>(this.getUrl);
+  }
+
+  sendAbout(newText: string): Observable<any> {
+    return this.http.post(this.postUrl, { body: newText });
+  }
+  // private apiUrl = 'https://jsonplaceholder.typicode.com/posts/4';
+  // private postUrl = 'https://jsonplaceholder.typicode.com/posts';// Замінити на реальну адресу API
+  //
+  // constructor(private http: HttpClient) {}
+  //
+  // getAboutMe(): Observable<{ body: string }> {
+  //   return this.http.get<{ body: string }>(this.apiUrl).pipe(
+  //     catchError(this.handleError)
+  //   );
+  // }
+  //
+  // sendAboutMe(newText: string): Observable<any> {
+  //   const body = { title: 'About Me', body: newText };
+  //   return this.http.post<any>(this.postUrl, body).pipe(
+  //     catchError(this.handleError)
+  //   );
+  // }
+  // getAboutText(): Observable<string> {
+  //   return this.http.get<any>(this.apiUrl).pipe(
+  //     map(response => response.body || 'Дані не знайдено') // API повертає об'єкт, тому беремо body
+  //   );
+  // }
+  // // Виконуємо GET-запит для отримання даних
+  // getResumeData(): Observable<any> {
+  //   return this.http.get<any>(this.apiUrl);
+  //
+  // }
+
+  // Виконуємо POST-запит для надсилання даних
+  // sendResumeData(data: any): Observable<any> {
+  //   return this.http.post<any>(this.apiUrl, data);
+  // }
+}
